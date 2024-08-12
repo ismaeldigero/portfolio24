d56eef96b4e773735cb95c581949dc48a3af13d2
ismaeldigero <128565715+ismaeldigero@users.noreply.github.com>
2024-08-12 10:17:17 -0600

    v.1.00.00

---
+++ script.js
@@ -0,0 +1,16 @@
+gsap.registerPlugin(ScrollTrigger);
+
+var frame_count  = 9,
+    offset_value = 100;
+
+gsap.to(".viewer", {
+  backgroundPosition: (-offset_value * frame_count * 2) + "px 50%",
+  ease: "steps(" + frame_count + ")", // use a stepped ease for the sprite sheet
+  scrollTrigger: {
+    trigger: ".scene",
+    start: "top top",
+    end: "+=" + (frame_count * offset_value),
+    pin: true,
+    scrub: true
+  }
+});
